014b4130365ccdbde65d48980c4ffbc2c2d4fd9b
tomcocogold <97610559+tomcocogold@users.noreply.github.com>
2022-05-28 18:49:00 +0200

    advert_window first draft

--- advert_window.py
+++ advert_window.py
@@ -3,17 +3,21 @@ import register_window
 import account_window
 import mysql.connector as sql
 
+import search_engine
+
 
 def everything():
     color = "#29fbc1"
 
-    def login():
-        login_window.destroy()
-        account_window.everything()
+    def return_button():
+        advert_window.destroy()
+        search_engine.everything()
 
-    def register():
-        login_window.destroy()
-        register_window.everything()
+    def sign_up():
+        pass
+
+    def add_opinion():
+        pass
 
     advert_window = Tk()
 
@@ -31,23 +35,49 @@ def everything():
     advert_window.config(background=color)
     advert_window.resizable(FALSE, FALSE)
 
-    frame1 = Frame(advert_window)  # dane użytkownika
-    frame1.pack()
-    # register
-    Label(frame1, text="Dane użytkownika", font=("Arial", 20), bg=color, pady=30).grid(row=0, column=0, sticky="nsew")
+    frame = Frame(advert_window, bg=color)
+    frame.pack()
+    subject = Label(frame, text="Przemiot", font=("Arial", 10), bg=color)
+    price = Label(frame, text="Cena [zł/h]", font=("Arial", 10), bg=color)
+    duration = Label(frame, text="Czas lekcji [min]", font=("Arial", 10), bg=color)
+    mode = Label(frame, text="Tryb", font=("Arial", 10), bg=color)
+    description = Label(frame, text="Opis", font=("Arial", 10), bg=color)
+    links = Label(frame, text="Linki", font=("Arial", 10), bg=color)
+    films = Label(frame, text="Filmy", font=("Arial", 10), bg=color)
+    images = Label(frame, text="Ilustracje", font=("Arial", 10), bg=color)
+    subject_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    price_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    duration_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    mode_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    description_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    links_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    films_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    images_entry = Label(frame, text="", font=("Arial", 14), bg="white", relief="sunken")
+    return_button = Button(frame, text="Wróć", command=return_button)
+    sign_up_button = Button(frame, text="Zapisz się", command=sign_up)
+    add_opinion_button = Button(frame, text="Dodaj opinię", command=add_opinion)
 
-    # Name
-    Label(frame1, text="Imię", font=('Arial', 10), bg=color, pady=5).grid(row=5, column=0, sticky="nsew")
-    Label(frame1, text="Imię", font=("Arial", 14), fg="black", bg="white").grid(row=10, column=0, sticky="nsew")
+    subject.grid(row=0, column=0)
+    price.grid(row=5, column=0)
+    duration.grid(row=10, column=0)
+    mode.grid(row=15, column=0)
+    description.grid(row=20, column=0)
+    links.grid(row=25, column=0)
+    films.grid(row=30, column=0)
+    images.grid(row=35, column=0)
 
-    # Surname
-    Label(frame1, text="Nazwisko", font=('Arial', 10), bg=color, pady=5).grid(row=15, column=0, sticky="nsew")
-    Label(frame1, text="Nazwisko", font=("Arial", 14), fg="black", bg="white").grid(row=20, column=0, sticky="nsew")
+    subject_entry.grid(row=0, column=1, sticky='ew')
+    price_entry.grid(row=5, column=1, sticky='ew')
+    duration_entry.grid(row=10, column=1, sticky='ew')
+    mode_entry.grid(row=15, column=1, sticky='ew')
+    description_entry.grid(row=20, column=1, sticky='ew')
+    links_entry.grid(row=25, column=1, sticky='ew')
+    films_entry.grid(row=30, column=1, sticky='ew')
+    images_entry.grid(row=35, column=1, sticky='ew')
 
-    # email
-    Label(frame1, text="Email", font=('Arial', 10), bg=color, pady=5).grid(row=25, column=0, sticky="nsew")
-    Label(frame1, text="Email", font=("Arial", 14), fg="black", bg="white").grid(row=30, column=0, sticky="nsew")
-    Label(frame1, text="Rodzaj konta", font=('Arial', 10), bg=color, pady=5).grid(row=35, column=0, sticky="nsew")
-    Label(frame1, text="Uczeń", font=("Arial", 14), fg="black", bg="white").grid(row=40, column=0, sticky="nsew")
+    return_button.grid(row=40, column=0)
+    sign_up_button.grid(row=40, column=1)
+    add_opinion_button.grid(row=45, column=1)
 
     advert_window.mainloop()
+
